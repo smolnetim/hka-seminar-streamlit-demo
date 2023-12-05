49d9b5842a0c3240b3982a2456b36cffde368b47
Tim Smolne <larry@usa-life.net>
2023-12-05 16:58:28 +0100

    updated example

--- frontend/test-app/Willkommen.py
+++ frontend/test-app/Willkommen.py
@@ -2,12 +2,7 @@
 import streamlit as st
 st.write("# Hallo Welt 👋🏻")
 
-# Dataset als Tabelle ausgeben
-import seaborn as sns
-df = sns.load_dataset('mpg')
-st.dataframe(df)
-
-# Als Graph ausgeben
-import plotly.express as px
-fig = px.scatter(df, x='horsepower', y='mpg', color='origin')
-st.plotly_chart(fig)
+import pandas as pd
+import numpy as np
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+st.line_chart(chart_data)
